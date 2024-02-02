99f4069ff59fb1f7216feda8a2c8bf24beeb3f8a
Sergey-Ivanovich <ericnoata@gmail.com>
2024-02-02 19:35:01 +0000

    began implemented api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 function cityHandle(event) {
   event.preventDefault();
+
+  let apiKey = `oe3107c03bbf1b061844a8c3d518t9b3`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
 }
 
 let cityForm = document.querySelector("#city-form");

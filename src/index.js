514b219dc05cb9a9f321dd2bbd5925128dacf4a8
Sergey-Ivanovich <ericnoata@gmail.com>
2024-02-02 20:42:55 +0000

    added basic search engine and display temp changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function weatherRefresh(response) {
+  let tempElement = document.querySelector("#shownTemp");
+  let cityNameElement = document.querySelector("h1");
+  let currentTemp = Math.round(response.data.temperature.current);
+
+  cityNameElement.innerHTML = response.data.city;
+  tempElement.innerHTML = `${currentTemp} `;
+}
+
+function apiSearch(city) {
+  let apiKey = `oe3107c03bbf1b061844a8c3d518t9b3`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(weatherRefresh);
+}
+
 function cityHandle(event) {
   event.preventDefault();
 
-  let apiKey = `oe3107c03bbf1b061844a8c3d518t9b3`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
+  let city = document.querySelector(`#city-box`);
+  apiSearch(city.value);
 }
 
 let cityForm = document.querySelector("#city-form");
